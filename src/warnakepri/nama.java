fe9eb4d8ef34fcdb8ce1e0ec001b770f29f65973
fiqriakbar9 <42399322+fiqriakbar9@users.noreply.github.com>
2019-10-15 19:24:02 +0700

    Add files via upload

---
+++ src/warnakepri/nama.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package warnakepri;
+
+/**
+ *
+ * @author 'KoalaX'
+ * NAMA     : Fiqri Akbar Pratama
+ * KELAS    : PBO1/IF1
+ * NIM      : 10118020
+ * Deksripsi Program : Program ini berisi program untuk menampilkan kepribadian
+ * anda melalui warna yang anda pilih yang sudah berbasis objek oriented
+ */
+public class nama {
+    
+    public String namaMu;
+
+    public void tampilNama(String namamu) {
+        System.out.println("\n====HASIL TEST KEPRIBADIAN "
+                + namaMu.toUpperCase() + "====");
+        System.out.println("");
+    }
+}
